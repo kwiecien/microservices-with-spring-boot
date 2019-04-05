cec6049d505afa93e4d39df3e148ae9b5b76b630
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-05 12:41:19 +0200

    remove unneeded imports

--- src/main/java/at/iteratec/sample/Application.java
+++ src/main/java/at/iteratec/sample/Application.java
@@ -2,9 +2,7 @@ package at.iteratec.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,8 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableEurekaClient
 @EnableFeignClients
-@EnableCircuitBreaker
-@EnableHystrix
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
