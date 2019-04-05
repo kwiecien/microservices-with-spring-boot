1aada4da31fd020c2f362872d5eab78944512a1b
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-05 12:14:30 +0200

    add hystrix and circuit breaker

--- src/main/java/at/iteratec/sample/Application.java
+++ src/main/java/at/iteratec/sample/Application.java
@@ -2,7 +2,9 @@ package at.iteratec.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,6 +12,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrix
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/at/iteratec/sample/LoggingService.java
+++ src/main/java/at/iteratec/sample/LoggingService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "logging")
+@FeignClient(value = "logging", fallback = LoggingServiceFallback.class)
 public interface LoggingService {
     @PostMapping("/api/log/{name}")
     void log(@PathVariable String name, @RequestBody String expression);
